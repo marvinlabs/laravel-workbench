aeba47b61b490748ee45732a8cf9b6556429a077
Vincent Mimoun-Prat <vpratfr@gmail.com>
2017-11-13 09:50:38 +0100

    Add data attributes on alert

--- resources/views/laravel-html-bootstrap-4/alert.blade.php
+++ resources/views/laravel-html-bootstrap-4/alert.blade.php
@@ -21,13 +21,17 @@
 
         @component('bs::alert', ['type' => 'dark'])
             @slot('heading')
-                An alert with heading
+            An alert with heading
             @endslot
 
             <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit
                longer so that you can see how spacing within an alert works with this kind of content.</p>
             <hr>
             <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+        @endcomponent
+
+        @component('bs::alert', ['type' => 'warning', 'data' => ['alert-id' => 40, 'context' => 'sample-code']])
+            An alert with some data attributes
         @endcomponent
 
     </div>
