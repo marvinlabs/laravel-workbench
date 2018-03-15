b0202a1c70f3f9e41d82f59f63a93ac89332b49e
Vincent Mimoun-Prat <vpratfr@gmail.com>
2018-03-15 16:32:55 +0100

    Add a presenter to generate tab navigation

--- menus/bootstrap4.php
+++ menus/bootstrap4.php
@@ -4,11 +4,12 @@ use MarvinLabs\Menus\Facades\Menu;
 use MarvinLabs\Menus\Menu\Items\Heading;
 use MarvinLabs\Menus\Menu\Items\Link;
 use MarvinLabs\Menus\Presenters\Bootstrap4\NavbarNavPresenter;
+use MarvinLabs\Menus\Presenters\Bootstrap4\TabsNavPresenter;
 use MarvinLabs\Menus\Support\Links\RouteLink;
 use MarvinLabs\Menus\Support\Links\UrlLink;
 use MarvinLabs\Menus\Support\Links\VoidLink;
 
-Menu::make('navbar-nav', function(\MarvinLabs\Menus\Menu\Menu $menu) {
+Menu::make('main-menu', function(\MarvinLabs\Menus\Menu\Menu $menu) {
     $menu->setPresenter(new NavbarNavPresenter('ml-auto'));
 
     $menu->addLink('Item 1', new VoidLink());
@@ -18,5 +19,21 @@ Menu::make('navbar-nav', function(\MarvinLabs\Menus\Menu\Menu $menu) {
         $item->addDivider();
         $item->addLink('Item 2.3', new RouteLink('demo.menu', ['category' => 'bs4']));
     });
-    $menu->addLink('Item 3', new RouteLink('demo.menu', ['category' => 'bs4']));
+    $menu->addLink('Item 3', new UrlLink('/test'));
+});
+
+Menu::make('tab-menu-1', function(\MarvinLabs\Menus\Menu\Menu $menu) {
+    $menu->setPresenter(new TabsNavPresenter());
+
+    $menu->addLink('Tab 1', new VoidLink())->id('tab_1');
+    $menu->addLink('Tab 2', new VoidLink())->id('tab_2');
+    $menu->addLink('Tab 3', new VoidLink())->id('tab_3');
+});
+
+Menu::make('tab-menu-2', function(\MarvinLabs\Menus\Menu\Menu $menu) {
+    $menu->setPresenter(new TabsNavPresenter(TabsNavPresenter::TYPE_PILLS));
+
+    $menu->addLink('Tab A', new VoidLink())->id('tab_A');
+    $menu->addLink('Tab B', new VoidLink())->id('tab_B');
+    $menu->addLink('Tab C', new VoidLink())->id('tab_C');
 });
--- resources/views/laravel-menus/bs4.blade.php
+++ resources/views/laravel-menus/bs4.blade.php
@@ -4,20 +4,64 @@
 
 @section('page.body')
 
-    <h1>Bootstrap 4 presenter</h1>
-
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <h1 class="my-3">Bootstrap 4 NavBar Nav</h1>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark my-3">
             <a href="#" class="navbar-brand">Navbar</a>
 
-            {!! Menu::render('navbar-nav') !!}
+            {!! Menu::render('main-menu') !!}
         </nav>
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary my-3">
             <a href="#" class="navbar-brand">Navbar</a>
 
-            @menu('navbar-nav')
+            @menu('main-menu')
         </nav>
+
+        <h1 class="my-3">Bootstrap 4 Navs</h1>
+
+        <?php use MarvinLabs\Menus\Presenters\Bootstrap4\NavPresenter; ?>
+
+        <div class="my-3">
+            @menu('main-menu', new NavPresenter('nav'))
+        </div>
+
+        <div class="my-3">
+            @menu('main-menu', new NavPresenter('nav nav-pills'))
+        </div>
+
+        <h1 class="my-3">Bootstrap 4 Tabs</h1>
+
+        <div class="my-3">
+            @menu('tab-menu-1')
+            <div class="tab-content p-3">
+                <div class="tab-pane fade show active" id="nav_tab_1" role="tabpanel" aria-labelledby="tab_tab_1">
+                    This is first tab
+                </div>
+                <div class="tab-pane fade" id="nav_tab_2" role="tabpanel" aria-labelledby="tab_tab_2">
+                    This is second tab
+                </div>
+                <div class="tab-pane fade" id="nav_tab_3" role="tabpanel" aria-labelledby="tab_tab_3">
+                    This is third tab
+                </div>
+            </div>
+        </div>
+
+        <div class="my-3">
+            @menu('tab-menu-2')
+            <div class="tab-content p-3">
+                <div class="tab-pane active" id="nav_tab_A" role="tabpanel" aria-labelledby="tab_tab_A">
+                    This is tab A
+                </div>
+                <div class="tab-pane fade" id="nav_tab_B" role="tabpanel" aria-labelledby="tab_tab_B">
+                    This is tab B
+                </div>
+                <div class="tab-pane fade" id="nav_tab_C" role="tabpanel" aria-labelledby="tab_tab_C">
+                    This is tab C
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
